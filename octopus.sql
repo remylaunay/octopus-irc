0a5688220ed2d7a69289036e90cf6ffca6f43395
remylaunay <remylaunay@gmail.com>
2016-06-20 18:44:01 +0200

    Update octopus.sql

--- octopus.sql
+++ octopus.sql
@@ -1,24 +1,23 @@
--- phpMyAdmin SQL Dump
--- version 4.2.12deb2+deb8u1
--- http://www.phpmyadmin.net
 --
--- Client :  localhost
--- Généré le :  Dim 19 Juin 2016 à 21:18
--- Version du serveur :  5.5.49-0+deb8u1
--- Version de PHP :  5.6.20-0+deb8u1
+-- Structure de la table `chanlist`
+--
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+CREATE TABLE IF NOT EXISTS `chanlist` (
+`id` int(11) NOT NULL,
+  `chan` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+-- --------------------------------------------------------
 
 --
--- Base de données :  `octopus`
+-- Structure de la table `closelist`
 --
+
+CREATE TABLE IF NOT EXISTS `closelist` (
+`id` int(11) NOT NULL,
+  `chan` text NOT NULL,
+  `reason` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -34,16 +33,38 @@ CREATE TABLE IF NOT EXISTS `members` (
   `current_uid` text NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Contenu de la table `members`
+-- Structure de la table `online`
 --
 
-INSERT INTO `members` (`id`, `login`, `code`, `level`, `current_uid`) VALUES
-(1, 'Test', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', '5', '');
+CREATE TABLE IF NOT EXISTS `online` (
+`id` int(11) NOT NULL,
+  `nick` text NOT NULL,
+  `uid` text NOT NULL,
+  `user` text NOT NULL,
+  `host` text NOT NULL,
+  `vhost` text NOT NULL,
+  `real` text NOT NULL,
+  `start` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=latin1;
 
 --
 -- Index pour les tables exportées
 --
+
+--
+-- Index pour la table `chanlist`
+--
+ALTER TABLE `chanlist`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `closelist`
+--
+ALTER TABLE `closelist`
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `members`
@@ -52,14 +73,7 @@ ALTER TABLE `members`
  ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT pour les tables exportées
+-- Index pour la table `online`
 --
-
---
--- AUTO_INCREMENT pour la table `members`
---
-ALTER TABLE `members`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE `online`
+ ADD PRIMARY KEY (`id`);
