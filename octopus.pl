9e1d9f5f19c92cd2f7fc412c4104f8948953892c
remylaunay <remylaunay@gmail.com>
2016-06-29 16:17:37 +0200

    Update octopus.pl

--- octopus.pl
+++ octopus.pl
@@ -42,13 +42,13 @@ $mysql{"DB"} = "octopus";
 #NE PAS TOUCHER A CETTE PARTIE#
 ###############################
 
-		  $SIG{HUP} = sub {
+	$SIG{HUP} = sub {
     	  print "got SIGHUP\n";
     	  delete $INC{"Octopus.pm"};
     	  delete $INC{"Service.pm"};
     	  require "Octopus.pm";
     	  require "Service.pm";
-  		};
+  	};
 
 print "================ Octopus IRC Service ================\n";
 print "\n";
@@ -58,4 +58,4 @@ print "\n";
 print "================ Octopus IRC Service ================\n\n";
 
 
-Service->init($link{"SERV"},$link{"PASS"},$link{"ADDR"},$link{"PORT"},$link{"DESC"},$link{"SID"},$octopus{"NICK"},$octopus{"USER"},$octopus{"HOST"},$octopus{"NAME"},$octopus{"CHAN"},$mysql{"HOST"},,$mysql{"PORT"},$mysql{"LOGIN"},$mysql{"PASS"},$mysql{"DB"});
+Service::init($link{"SERV"},$link{"PASS"},$link{"ADDR"},$link{"PORT"},$link{"DESC"},$link{"SID"},$octopus{"NICK"},$octopus{"USER"},$octopus{"HOST"},$octopus{"NAME"},$octopus{"CHAN"},$mysql{"HOST"},,$mysql{"PORT"},$mysql{"LOGIN"},$mysql{"PASS"},$mysql{"DB"});
